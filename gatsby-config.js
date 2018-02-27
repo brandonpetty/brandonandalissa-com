fcd5c58a6bed586dadccb75df1148a6d1ca50d2b
Alissa Taylor <ataylor@hirezstudios.com>
2018-02-26 23:08:10 -0500

    add: google analytics plugin and tag

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
+    title: `Brandon and Alissa`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -9,6 +9,16 @@ module.exports = {
       resolve: `gatsby-plugin-google-fonts`,
       options: {
         fonts: [`Open Sans`, `Lato`]
+      }
+    },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+    options: {
+      trackingId: "UA-114819795-1",
+      // Puts tracking script in the head instead of the body
+      head: false,
+      // Setting this parameter is optional
+      anonymize: true,
       }
     }
   ],
